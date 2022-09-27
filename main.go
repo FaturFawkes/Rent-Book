dccc0341fe7f2f5e7dc2c042da59b3ff6ee72a71
LukmanNgoding <allukman.lukman99@gmail.com>
2022-09-27 09:39:13 +0800

    Update view login

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"os/exec"
 	"rentbook/model"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
-
 
 func migrate(db *gorm.DB) {
 	db.AutoMigrate(&model.Buku{})
@@ -41,9 +41,22 @@ func Login() (username string, password string) {
 	return us, pw
 }
 
+func Register() (nama, email, password string) {
+	var na, em, pas string
+	fmt.Println("Masukkan User Name : ")
+	fmt.Scanln(&na)
+	fmt.Println("Masukkan Email : ")
+	fmt.Scanln(&em)
+	fmt.Println("Masukkan Password : ")
+	fmt.Scanln(&pas)
+
+	return na, em, pas
+}
+
 func main() {
 	var isRunning bool = true
 	var inputBook int
+	var inputBook2 int
 	_, err := conn()
 	if err != nil {
 		fmt.Println("error")
@@ -51,24 +64,61 @@ func main() {
 		fmt.Println("sukses")
 	}
 
-	if isRunning {
+	for isRunning {
 		fmt.Println("\t ---- Rent Book ----")
-		fmt.Println("Login")
-		fmt.Scanln(Login())
-		fmt.Println("Login Berhasil => ENTER")
-		fmt.Scanln(&inputBook)
-		fmt.Println("2. Search Buku")
-		fmt.Println("3. Keseluruhan Buku")
-		fmt.Println("4. Tambah Buku")
-		fmt.Println("5. Pinjam Buku")
-		fmt.Println("6. Pencarian Buku")
-		fmt.Println("7. Detail Pinjam Buku")
-		fmt.Println("8. Pengembalian Buku")
-		fmt.Println("9. Logout")
-		fmt.Println("Masukkan Input")
+		fmt.Println("1. Login")
+		fmt.Println("2. Register")
+		fmt.Println("3. Search Buku")
+		fmt.Println("4. Keseluruhan Buku\n")
+		fmt.Println("\tMasukkan Piliha Anda")
 		fmt.Scanln(&inputBook)
 		switch inputBook {
+		case 1:
+			fmt.Println("Silahkan Login\n")
+			Login()
+			fmt.Println("\tLogin Berhasil\n")
 		case 2:
+			fmt.Println("Silahkan Registrasi\n")
+			Register()
+			fmt.Println("\tRegister Berhasil\n")
+		}
+		fmt.Println("3. Search Buku")
+		fmt.Println("4. Keseluruhan Buku")
+		fmt.Println("5. Tambah Buku")
+		fmt.Println("6. Pinjam Buku")
+		fmt.Println("7. Pencarian Buku")
+		fmt.Println("8. Detail Pinjam Buku")
+		fmt.Println("9. Pengembalian Buku")
+		fmt.Println("10. Logout")
+		fmt.Println("Masukkan Input")
+		fmt.Scanln(&inputBook)
+		Logout()
+		fmt.Println("1. Login")
+		fmt.Println("2. Register")
+		fmt.Println("3. Search Buku")
+		fmt.Println("4. Keseluruhan Buku\n")
+		fmt.Println("\tMasukkan Piliha Anda")
+		Login()
+		fmt.Scanln(&inputBook2)
+		fmt.Println("3. Search Buku")
+		fmt.Println("4. Keseluruhan Buku")
+		fmt.Println("5. Tambah Buku")
+		fmt.Println("6. Pinjam Buku")
+		fmt.Println("7. Pencarian Buku")
+		fmt.Println("8. Detail Pinjam Buku")
+		fmt.Println("9. Pengembalian Buku")
+		fmt.Println("10. Logout")
+		fmt.Println("Masukkan Input")
+		fmt.Scanln(&inputBook2)
+		switch inputBook2 {
+		case 1:
+			fmt.Println("Silahkan Login\n")
+			Login()
+			fmt.Println("\tLogin Berhasil\n")
+		case 2:
+			fmt.Println("Silahkan Registrasi\n")
+			Register()
+			fmt.Println("\tRegister Berhasil\n")
 		case 3:
 		case 4:
 		case 5:
@@ -76,24 +126,27 @@ func main() {
 		case 7:
 		case 8:
 		case 9:
+		case 10:
 			Logout()
-			isRunning = false
-			fmt.Println("\t---Sampai Berjumpa Kembali---")
-			fmt.Println("\t ---- Rent Book ----")
-			fmt.Println("Login")
-			fmt.Scanln(Login())
-			fmt.Println("Login Berhasil => ENTER")
-			fmt.Scanln(&inputBook)
-			fmt.Println("2. Search Buku")
-			fmt.Println("3. Keseluruhan Buku")
-			fmt.Println("4. Tambah Buku")
-			fmt.Println("5. Pinjam Buku")
-			fmt.Println("6. Pencarian Buku")
-			fmt.Println("7. Detail Pinjam Buku")
-			fmt.Println("8. Pengembalian Buku")
-			fmt.Println("9. Logout")
-			fmt.Println("Masukkan Input")
-			fmt.Scanln(&inputBook)
+			// fmt.Println("Login Berhasil => ENTER")
+			// fmt.Scanln(&inputBook2)
+			// fmt.Println("\t ---- Rent Book ----")
+			// fmt.Println("1. Login")
+			// fmt.Println("2. Register")
+			// fmt.Println("3. Search Buku")
+			// fmt.Println("4. Keseluruhan Buku\n")
+			// fmt.Println("\tMasukkan Piliha Anda")
+			// fmt.Scanln(&inputBook2)
+			// fmt.Println("3. Search Buku")
+			// fmt.Println("4. Keseluruhan Buku")
+			// fmt.Println("5. Tambah Buku")
+			// fmt.Println("6. Pinjam Buku")
+			// fmt.Println("7. Pencarian Buku")
+			// fmt.Println("8. Detail Pinjam Buku")
+			// fmt.Println("9. Pengembalian Buku")
+			// fmt.Println("10. Logout")
+			// fmt.Println("Masukkan Input")
+			// fmt.Scanln(&inputBook2)
 		}
 	}
 }
