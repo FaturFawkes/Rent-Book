1a4beef32e14e85f5f67ca08a4af255dcedc7b10
LukmanNgoding <allukman.lukman99@gmail.com>
2022-09-28 14:08:59 +0800

    Update buku belum dipinjam

--- model/Rent.go
+++ model/Rent.go
@@ -2,13 +2,14 @@ package model
 
 import (
 	"time"
+
 	"gorm.io/gorm"
 )
 
 type Rent struct {
 	gorm.Model
-	Id_user     int `gorm:"type:int(11)"`
-	Id_buku     int `gorm:"type:int(11)"`
+	Id_user     int    `gorm:"type:int(11)"`
+	Id_buku     int    `gorm:"type:int(11)"`
 	Pemilik     string `gorm:"type:varchar(255)"`
 	Judul       string `gorm:"type:varchar(50)"`
 	Tgl_pinjam  time.Time
@@ -19,10 +20,17 @@ type RentModel struct {
 	DB *gorm.DB
 }
 
+// db.Session(&gorm.Session{QueryFields: true}).Find(&user)
 func (rm RentModel) GetAll() ([]Rent, error) {
-
+	var res []Rent
+	err := rm.DB.Session(&gorm.Session{QueryFields: true}).Model(&Rent{}).Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 
-func (rm RentModel) AddRent() (Rent, error) {
+// Select("id_user", "id_buku", "pemilik", "judul").Model(&Rent{}).Find(&res).Error
+// func (rm RentModel) AddRent() (Rent, error) {
 
-}
+// }
